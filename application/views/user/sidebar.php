4a8c09f29be3fca6d039d6eef04983049fb79c7f
rony-chan <ronydtorch@yahoo.com>
2015-01-24 20:51:22 +0700

    verifikasi akun di folder nonresource

--- application/views/user/sidebar.php
+++ application/views/user/sidebar.php
@@ -7,7 +7,6 @@
  */
 ?>
 <br/><br/>
-<strong>Iklan</strong><br/>
 <a href="#" target="_blank"><img class="adsimg" src="<?php echo base_url('resource/img/banner/ads1.jpg');?>"/></a>
 <br/><br/>
 <a href="#" target="_blank"><img class="adsimg" src="<?php echo base_url('resource/img/banner/ads2.jpg');?>"/></a>
