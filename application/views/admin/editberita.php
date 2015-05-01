8cc40c37cc8c1b0dc0b13dd0e628b57deef24863
yussan <yussandeveloper@gmail.com>
2015-05-01 19:52:32 +0700

    add tiny mce

--- application/views/admin/editberita.php
+++ application/views/admin/editberita.php
@@ -1,3 +1,5 @@
+<script src="//tinymce.cachefly.net/4.1/tinymce.min.js"></script>
+<script>tinymce.init({selector:'textarea'});</script>
 <div class="row">
 	<div class="col-md-12">
 		<div class="left-sidebar col-md-2">
